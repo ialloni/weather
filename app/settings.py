d771af3e73922594a9634decb90b0345c53a12ba
ialloni <sdls2000@bk.ru>
2025-06-20 20:09:54 +0300

    add settings

---
+++ app/settings.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_PATH = Path(__file__).resolve().parent.parent / ".env"
+
+
+class Settings(BaseSettings):
+    WEATHER_API_KEY: str
+    CHECK_IP_URL: str
+
+    model_config = SettingsConfigDict(env_file=ENV_PATH)
+
+
+settings = Settings()  # type: ignore
